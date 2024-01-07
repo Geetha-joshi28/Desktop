ed95330bec0ab1b449b0fda43af6e156f488cd56
Geetha-joshi28 <geethajoc250@gmail.com>
2024-01-07 18:48:39 +0530

    Basics

---
+++ mon.js
@@ -0,0 +1,39 @@
+
+
+  // const buttonElement = document.getElementById("button")
+  // buttonElement.addEventListener("click", () => {
+  //    buttonElement.style.color = "red";
+  //   console.log("i have clicked button.. waht next");
+
+  //    // Add event listener for mouseover (hover)
+  //   buttonElement.addEventListener("mouseover", () => {
+  //     // Change the background color to yellow using the DOM
+  //     buttonElement.style.backgroundColor = "pink";
+  //   });
+
+  //   // Add event listener for mouseout (hover out)
+  //   buttonElement.addEventListener("mouseout", () => {
+  //     // Restore the initial background color
+  //     buttonElement.style.backgroundColor = "lightgray";
+  //   });
+  // })
+
+  // fullName = "geetha joshi";
+  // age = 24;
+  // price = 99.99;
+  // x = null;
+  // y = undefined;
+  // console.log(fullName);
+// for(let count=1; count<=100; count++) {
+//   console.log("apna college");
+// }
+
+// console.log("loop has ended");
+
+let str = "presidencyCollege";
+ let str1=str.toUpperCase();
+console.log(str);
+  
+
+
+
